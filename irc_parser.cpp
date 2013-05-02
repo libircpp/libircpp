f6752a774904b9af79cd227981b0d57dbe6caec6
Joseph Dobson <dobson@localhost.localdomain>
2013-05-02 16:49:37 +0100

    started getting it to parse the recipiant

--- irc_parser.cpp
+++ irc_parser.cpp
@@ -6,6 +6,38 @@
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_bind.hpp>
 
+#include <boost/fusion/include/adapt_struct.hpp>
+
+#include <string>
+#include <ostream>
+
+struct prefix { optional_string nick, user, host; };
+
+std::ostream& operator<<(std::ostream& os, const prefix& pfx) {
+	if(pfx.nick) os << '<' << *pfx.nick << '>';
+
+	if(pfx.nick && pfx.user) os << "!";
+
+	if(pfx.user) os << '<' << *pfx.user << '>';
+
+	if(pfx.host 
+	&&( pfx.nick || pfx.user )) os << "@";
+
+	if(pfx.host) os << '<' <<  *pfx.host << '>';
+	return os;
+}
+
+void prnt_pfx(const prefix& pfx) {
+	std::cout << pfx << '\n';
+}
+
+BOOST_FUSION_ADAPT_STRUCT(
+	::prefix,
+	(optional_string, nick)
+	(optional_string, user)
+	(optional_string, host)
+)
+
 namespace qi =boost::spirit::qi;
 namespace phx=boost::phoenix;
 
@@ -18,21 +50,36 @@ void irc_parser::parse_message(const std::string& message) {
 	qi::lit_type    lit;
 	qi::char_type   char_;
 	qi::space_type  space;
-	qi::string_type string;
 	qi::_1_type     _1;
 	qi::_2_type     _2;
 	qi::_3_type     _3;
 
-	rule<std::string> word =+(~char_(' '));
+	qi::alpha_type  alpha;
+	qi::alnum_type  alnum;
+
+	qi::lexeme_type lexeme;
+	qi::attr_type   attr;
+
+ 
+	rule<std::string> nick = alpha >> *( alnum | char_("-[]\\`^{}") );
+
+	rule<std::string> host = +~char_(' ');
+ 	//RFC 1459 is really vague about user, but no "?" seems like the best way to to deal with it
+	rule<std::string> user = +~char_(" ?@");
+
+	rule<std::string> word =+~char_(' ');
 	rule<std::string> line =lit(':') >> *char_;
 
-	rule<std::string> actor=char_("@#") >> word;
-
+	rule<prefix> prefix_parser = lit(':') >> ( nick                    >> -( '!' >> user )        >> -( '@' >> host ) >> lexeme[ ' ' ]
+			                                 | attr(optional_string{}) >> attr(optional_string{}) >> host             >> lexeme[ ' ' ]
+			                                 ) 
+											 ;
+		 
 	bool success=qi::phrase_parse(
 		first, last,
 
-		-(lit(':') >> word) >> //prefix
-			( ("PRIVMSG" >> +(!lit(':') >> word) >> line)  [ phx::bind(phx::ref(on_privmsg), _1, _2)     ]
+		 -prefix_parser[prnt_pfx] >> 			
+		    ( ("PRIVMSG" >> +(!lit(':') >> word) >> line)  [ phx::bind(phx::ref(on_privmsg), _1, _2)     ]
 			| ("NOTICE"  >> word >> line)                  [ phx::bind(phx::ref(on_notice),  _1, _2)     ]
 			| ("MODE"    >> word >> line)                  [ phx::bind(phx::ref(on_mode),    _1, _2)     ]
 			| ("TOPIC"   >> word >> line)                  [ phx::bind(phx::ref(on_topic),   _1, _2)     ]
