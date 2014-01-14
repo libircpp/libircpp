4b50b83f5140fae8ec6fa3418d6715f8072414ce
redtide <redtid3@gmail.com>
2014-01-14 16:16:41 +0100

    Moved application source files in their own src directory. Changed Makefile and ui|irc/util.hpp for the new directory tree.

--- util.hpp
+++ util.hpp
@@ -4,7 +4,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include "../util.hpp"
+#include "../src/util.hpp"
 
 /*
 #ifndef UTIL_HPP
