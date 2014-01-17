8a52941f99353ed41ea0d37b65198d80b2055da1
dobson <dobson@floyd>
2014-01-17 14:10:23 +0000

    added creation dates

--- command.cpp
+++ command.cpp
@@ -4,6 +4,8 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+// Created:     2014/01/17
+
 #include "command.hpp"
 
 #include <stdexcept>
--- command.hpp
+++ command.hpp
@@ -4,6 +4,8 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+//Created:     2014/01/17
+
 #ifndef IRC_CLIENT_COMMAND_HPP
 #define IRC_CLIENT_COMMAND_HPP
 
